0d721846a99194b888a5d2687ce867de4b926797
Cleyra Uzcategui <cleyra.uzcategui@gmail.com>
2018-09-26 15:47:33 +0100

    Added cardlist component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-import Card from './components/Card.js';
+import Card from './components/Card';
+import CardList from './components/Cardlist';
 
 class App extends Component {
   render() {
     return (
-      <Card/>
+      <React.Fragment>
+      <Card />
+      <CardList />
+    </React.Fragment>
     );
   };
 };

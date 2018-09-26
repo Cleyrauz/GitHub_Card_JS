7490697b72387f25b1589df9913cc24842b97f5c
Cleyra Uzcategui <cleyra.uzcategui@gmail.com>
2018-09-26 15:23:52 +0100

    Added component card

---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Card = (props) => {
+  return (
+  <div>
+    <img src="http://placehold.it/75" />
+    <div>Name here...</div>
+    <div>Company here...</div>
+  </div>
+);
+};
+
+export default Card;
